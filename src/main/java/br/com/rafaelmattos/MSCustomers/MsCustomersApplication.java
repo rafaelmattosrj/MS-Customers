500067f1dc92a466a725dcec96be4d70182f8b6b
Rafael Mattos <rafaelrj@live.com>
2020-09-22 15:12:56 -0300

    Created project

---
+++ src/main/java/br/com/rafaelmattos/MSCustomers/MsCustomersApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.MSCustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaelmattos/MSCustomers/MsCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.MSCustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
